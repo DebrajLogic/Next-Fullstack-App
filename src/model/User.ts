108eaf85b3251b69d3c5ba12c6a52d4cdc280ec0
DebrajLogic <debrajdey9696@gmail.com>
2024-04-09 12:41:32 -0400

    compleled user data modelling

--- src/model/User.ts
+++ src/model/User.ts
@@ -67,3 +67,8 @@ const userSchema : Schema<User> = new Schema({
 
     
 })
+
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>('User', userSchema))
+
+export default UserModel
